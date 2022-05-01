383998e6983fe270192ca186f7f2548a5e3c7fab
Winston Zheng <pa.winston2020@gmail.com>
2022-05-01 15:57:24 -0400

    Added some texts

--- BaseballStatistics/src/baseballstatistics/MultipleFileReader.java
+++
@@ -1,7 +0,0 @@
-/* 
-This will be used to read multiple game files and summarize the 
-statistics on each pitcher for a specified number of games. 
- */
-public class MultipleFileReader {
-
-}
---
+++ BaseballStatistics/src/baseballstatistics/MultipleFilesReader.java
@@ -0,0 +1,8 @@
+/* 
+Name: Winston Zheng
+Purpose: This will be used to read multiple game files and summarize the 
+statistics on each pitcher for a specified number of games. 
+ */
+public class MultipleFilesReader {
+
+}
