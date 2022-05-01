c21dc414b3ee70e8cca5e6470bbdbbe2918681a8
raybu <raybu@DESKTOP-RQJSTO0.home>
2022-05-01 12:01:11 -0400

    // added a few comments around date and commented out stats not needed

--- BaseballStatistics/src/baseballstatistics/Pitcher.java
+++ BaseballStatistics/src/baseballstatistics/Pitcher.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package baseballstatistics;
 
-/**
- *
- * @author raybu
- */
+
 public class Pitcher {
     // BRENDEN RAYBURN
     // Create a Pitcher class for the batting statistics for a player on the team.
