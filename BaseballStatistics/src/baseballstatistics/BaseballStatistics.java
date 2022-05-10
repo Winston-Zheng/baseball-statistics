1c25aa48e71170c53626d44b8ab9bef3d2f7e2c9
Winston Zheng <pa.winston2020@gmail.com>
2022-05-09 21:08:01 -0400

    Added full functionality to FileReaderHelper.java and MultipleFilesReader.java class

---
+++ BaseballStatistics/src/baseballstatistics/BaseballStatistics.java
@@ -0,0 +1,17 @@
+/*
+Team members: Winston Zheng, Dorian Earl, Joshua Fent
+ */
+package baseballstatistics;
+
+import baseballstatistics.PitcherSQL;
+import baseballstatistics.UserInput;
+import javafx.application.Application;
+
+//The main class
+public class BaseballStatistics {
+
+    public static void main(String[] args) {
+        Application.launch(UserInput.class, args);
+    }
+
+}
